e9686aacf13317d79faec1dd130d0e63b8e5bada
Alexander Sotnev <83303127+Shugga939@users.noreply.github.com>
2022-06-29 19:45:27 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let list = document.querySelector('.list')
+
+document.addEventListener('click', (e)=> {
+  let currentItem = e.target.closest('.list__item')
+  let title = currentItem.querySelector('.list__title')
+  let spoiler = currentItem.querySelector('.list__spoiler')
+  let state = currentItem.querySelector('.list__state')
+  spoiler.classList.toggle('hide')
+  state.classList.toggle('list__state--open')
+  title.classList.toggle('list__title--open')
+})
